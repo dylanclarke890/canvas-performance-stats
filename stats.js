509dc12de07a25a697b7cc7a27d2bd6d7746ff97
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-29 05:43:35 +0000

    tidied up class

--- stats.js
+++ stats.js
@@ -1,197 +1,143 @@
-class Color {
-  constructor({ ...color } = {}) {
-    this.r = color.r;
-    this.g = color.g;
-    this.b = color.b;
-  }
-}
-
-class ColorScheme {
+class PanelColorScheme {
   constructor(bg, fg) {
     this.bg = bg;
     this.fg = fg;
   }
 }
 
-function assignStyles(element, styles) {
-  for (const styleName in styles) {
-    element.style[styleName] = styles[styleName];
+class Color {
+  constructor(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
   }
 }
 
 class Stats {
-  currentPanelIndex = 0;
-  maxPanels = 2;
-  colorSchemes = {
-    fps: new ColorScheme(
-      {
-        r: 16,
-        g: 16,
-        b: 48,
-      },
-      {
-        r: 0,
-        g: 255,
-        b: 255,
-      }
-    ),
-    ms: new ColorScheme(
-      {
-        r: 16,
-        g: 48,
-        b: 16,
-      },
-      {
-        r: 0,
-        g: 255,
-        b: 0,
-      }
-    ),
-    mem: new ColorScheme(
-      {
-        r: 48,
-        g: 16,
-        b: 26,
-      },
-      {
-        r: 255,
-        g: 0,
-        b: 128,
-      }
-    ),
+  static panelWidth = 106;
+  static panelHeight = 40;
+  static containerWidth = 110;
+  static containerHeight = 60;
+  static colorSchemes = {
+    fps: new PanelColorScheme(new Color(16, 16, 48), new Color(0, 255, 255)),
+    ms: new PanelColorScheme(new Color(16, 48, 16), new Color(0, 255, 0)),
+    mem: new PanelColorScheme(new Color(48, 16, 26), new Color(255, 0, 128)),
   };
 
-  constructor({ ...settings } = {}) {
+  #target = null;
+  #DOMElements = {};
+  #containerElementStyles = [];
+  #currentPanelIndex = 0;
+  #maxPanels = 2;
+
+  constructor({ containerElementStyles, target } = {}) {
+    this.#containerElementStyles = containerElementStyles;
+    this.#target = target;
+    this.#setup();
+  }
+
+  #setup() {
     const parent = document.createElement("div");
-    assignStyles(parent, {
+    this.#assignStyles(parent, {
       fontFamily: "Helvetica, Arial, sans-serif",
       textAlign: "left",
       fontSize: "9px",
       opacity: "0.9",
-      width: "80px",
+      width: `${Stats.containerWidth}px`,
+      height: `${Stats.containerHeight}px`,
       cursor: "pointer",
     });
-    parent.addEventListener("click", this.nextPanel, false);
+    parent.addEventListener("click", () => this.#nextPanel());
+    if (this.#containerElementStyles) this.#assignStyles(parent, this.#containerElementStyles);
+    if (this.#target) this.#target.appendChild(parent);
+    this.#DOMElements.parent = parent;
 
-    const fpsDiv = this.panelContainer("fps", "block", parent);
-    this.fpsDiv = fpsDiv;
-    const fpsText = this.panelText("fps", fpsDiv);
-    const [fpsCtx, fpsData] = this.panelCanvas(fpsDiv, this.colorSchemes.fps.bg);
-
-    const msDiv = this.panelContainer("ms", "none", parent);
-    this.msDiv = msDiv;
-    const msText = this.panelText("ms", msDiv);
-    const [msCtx, msData] = this.panelCanvas(msDiv, this.colorSchemes.ms.bg);
+    this.#createPanel("fps", true);
+    this.#createPanel("ms", false);
 
     try {
-      if (performance && performance.memory.totalJSHeapSize) this.maxPanels = 3;
+      if (performance && performance.memory.totalJSHeapSize) {
+        this.#createPanel("mem", false);
+        this.#maxPanels = 3;
+      }
     } catch (ex) {}
 
-    const memDiv = this.panelContainer("mem", "none", parent);
-    this.memDiv = memDiv;
-    const memText = this.panelText("mem", memDiv);
-    const [memCtx, memData] = this.panelCanvas(memDiv, this.colorSchemes.mem.bg);
-
-    if (settings.domElementStyles) assignStyles(parent, settings.domElementStyles);
-    if (settings.appendTo) settings.appendTo.appendChild(parent);
-
-    function drawPanelData(data, minVal, colorScheme) {
-      for (let i = 0; i < 30; i++)
-        for (let j = 0; j < 73; j++) {
-          const L = (j + i * 74) * 4;
-          data[L] = data[L + 4];
-          data[L + 1] = data[L + 5];
-          data[L + 2] = data[L + 6];
-        }
-
-      for (let i = 0; i < 30; i++) {
-        const L = (73 + i * 74) * 4;
-        if (i < minVal) {
-          data[L] = colorScheme.bg.r;
-          data[L + 1] = colorScheme.bg.g;
-          data[L + 2] = colorScheme.bg.b;
-        } else {
-          data[L] = colorScheme.fg.r;
-          data[L + 1] = colorScheme.fg.g;
-          data[L + 2] = colorScheme.fg.b;
-        }
-      }
-    }
-
-    let minFps = 1000,
-      maxFps = 0,
-      minMs = 1000,
-      maxMs = 0,
-      minMem = 1000,
-      maxMem = 0;
-
-    let framesThisSec = 0,
-      now = performance.now(),
-      last = now,
-      lastFrame = now;
-
-    this.domElement = parent;
-    this.update = function () {
-      framesThisSec++;
-      now = performance.now();
-      const ms = Math.round(now - last);
-      minMs = Math.min(minMs, ms);
-      maxMs = Math.max(maxMs, ms);
-      drawPanelData(msData.data, Math.min(30, 30 - (ms / 200) * 30), this.colorSchemes.ms);
-      msText.innerHTML = `<strong>${ms} MS</strong>(${minMs}-${maxMs})`;
-      msCtx.putImageData(msData, 0, 0);
-      last = now;
-      if (now > lastFrame + 1000) {
-        const fps = Math.round((framesThisSec * 1000) / (now - lastFrame));
-        minFps = Math.min(minFps, fps);
-        maxFps = Math.max(maxFps, fps);
-        drawPanelData(fpsData.data, Math.min(30, 30 - (fps / 100) * 30), this.colorSchemes.fps);
-        fpsText.innerHTML = `<strong>${fps} FPS</strong> (${minFps}-${maxFps})`;
-        fpsCtx.putImageData(fpsData, 0, 0);
-        if (this.maxPanels === 3) {
-          const mem = Math.round(performance.memory.usedJSHeapSize * 9.54e-7);
-          minMem = Math.min(minMem, mem);
-          maxMem = Math.max(maxMem, mem);
-          drawPanelData(memData.data, Math.min(30, 30 - mem / 2), this.colorSchemes.mem);
-          memText.innerHTML = `<strong>${mem} MEM</strong> (${minMem}-${maxMem})`;
-          memCtx.putImageData(memData, 0, 0);
-        }
-        lastFrame = now;
-        framesThisSec = 0;
-      }
-    };
+    this.minFps = 1000;
+    this.maxFps = 0;
+    this.minMs = 1000;
+    this.maxMs = 0;
+    this.minMem = 1000;
+    this.maxMem = 0;
+    this.framesThisSec = 0;
+    this.now = performance.now();
+    this.last = this.now;
+    this.lastFrame = this.now;
   }
 
-  panelContainer = (panelColor, display, appendTo) => {
+  #assignStyles(element, styles) {
+    for (const styleName in styles) element.style[styleName] = styles[styleName];
+  }
+
+  #drawPanelData(data, minVal, colorScheme) {
+    const width = Stats.panelWidth;
+    const height = Stats.panelHeight;
+
+    // moving existing data across one "column"
+    for (let i = 0; i < height; i++)
+      for (let j = 0; j < width - 1; j++) {
+        const currentByte = (j + i * width) * 4;
+        data[currentByte] = data[currentByte + 4];
+        data[currentByte + 1] = data[currentByte + 5];
+        data[currentByte + 2] = data[currentByte + 6];
+      }
+
+    // filling in "column" with new data.
+    for (let i = 0; i < height; i++) {
+      const currentByte = (width - 1 + i * width) * 4;
+      if (i < minVal) {
+        data[currentByte] = colorScheme.bg.r;
+        data[currentByte + 1] = colorScheme.bg.g;
+        data[currentByte + 2] = colorScheme.bg.b;
+      } else {
+        data[currentByte] = colorScheme.fg.r;
+        data[currentByte + 1] = colorScheme.fg.g;
+        data[currentByte + 2] = colorScheme.fg.b;
+      }
+    }
+  }
+
+  #panelContainer(panelColor, display, target) {
     const div = document.createElement("div");
-    assignStyles(div, {
-      backgroundColor: `rgb(${Math.floor(this.colorSchemes[panelColor].bg.r / 2)},${Math.floor(
-        this.colorSchemes[panelColor].bg.g / 2
-      )},${Math.floor(this.colorSchemes[panelColor].bg.b / 2)})`,
+    this.#assignStyles(div, {
+      backgroundColor: `rgb(${Math.floor(Stats.colorSchemes[panelColor].bg.r / 2)},${Math.floor(
+        Stats.colorSchemes[panelColor].bg.g / 2
+      )},${Math.floor(Stats.colorSchemes[panelColor].bg.b / 2)})`,
       padding: "2px 0px 3px 0px",
       display,
+      height: `${Stats.containerHeight}px`,
+      boxSizing: "border-box",
     });
-    appendTo.appendChild(div);
+    target.appendChild(div);
     return div;
-  };
+  }
 
-  panelText = (panelType, appendTo) => {
+  #panelText(panelType, target) {
     const div = document.createElement("div");
     div.innerHTML = `<strong>${panelType.toUpperCase()}</strong>`;
-    assignStyles(div, {
-      color: `rgb(${this.colorSchemes[panelType].fg.r},${this.colorSchemes[panelType].fg.g},${this.colorSchemes[panelType].fg.b})`,
+    this.#assignStyles(div, {
+      color: `rgb(${Stats.colorSchemes[panelType].fg.r},${Stats.colorSchemes[panelType].fg.g},${Stats.colorSchemes[panelType].fg.b})`,
       margin: "0px 0px 1px 3px",
     });
-    appendTo.appendChild(div);
+    target.appendChild(div);
     return div;
-  };
+  }
 
-  panelCanvas = (appendTo, bgColor) => {
+  #panelCanvas(bgColor, target) {
     const canv = document.createElement("canvas");
-    canv.width = 74;
-    canv.height = 30;
-    assignStyles(canv, { display: "block", marginLeft: "3px" });
-    appendTo.appendChild(canv);
+    canv.width = Stats.panelWidth;
+    canv.height = Stats.panelHeight;
+    this.#assignStyles(canv, { display: "block", marginLeft: "3px" });
+    target.appendChild(canv);
 
     const ctx = canv.getContext("2d");
     ctx.fillStyle = `rgb(${bgColor.r},${bgColor.g},${bgColor.b})`;
@@ -199,28 +145,85 @@ class Stats {
     const data = ctx.getImageData(0, 0, canv.width, canv.height);
 
     return [ctx, data];
-  };
+  }
 
-  nextPanel = () => {
-    this.currentPanelIndex++;
-    this.currentPanelIndex = this.currentPanelIndex == this.maxPanels ? 0 : this.currentPanelIndex;
+  #createPanel(name, isFirst) {
+    const display = isFirst ? "block" : "none";
+    const div = this.#panelContainer(name, display, this.#DOMElements.parent);
+    this.#DOMElements[name] = {};
+    this.#DOMElements[name].div = div;
+    this.#DOMElements[name].text = this.#panelText(name, div);
+    const [ctx, data] = this.#panelCanvas(Stats.colorSchemes[name].bg, div);
+    this.#DOMElements[name].ctx = ctx;
+    this.#DOMElements[name].data = data;
+  }
 
-    this.fpsDiv.style.display = "none";
-    this.msDiv.style.display = "none";
-    this.memDiv.style.display = "none";
+  #nextPanel() {
+    const { fps, ms, mem } = this.#DOMElements;
+    this.#currentPanelIndex =
+      ++this.#currentPanelIndex == this.#maxPanels ? 0 : this.#currentPanelIndex;
 
-    switch (this.currentPanelIndex) {
+    fps.div.style.display = "none";
+    ms.div.style.display = "none";
+    if (this.#maxPanels === 3) mem.div.style.display = "none";
+
+    switch (this.#currentPanelIndex) {
       case 0:
-        this.fpsDiv.style.display = "block";
+        fps.div.style.display = "block";
         break;
       case 1:
-        this.msDiv.style.display = "block";
+        ms.div.style.display = "block";
         break;
       case 2:
-        this.memDiv.style.display = "block";
+        if (this.#maxPanels === 3) mem.div.style.display = "block";
         break;
       default:
-        break;
+        throw Error("Panel index out of range.");
     }
-  };
+  }
+
+  update() {
+    const { ms, fps, mem } = this.#DOMElements;
+    this.now = performance.now();
+    this.framesThisSec++;
+
+    // Interval between frames in ms.
+    const msValue = Math.round(this.now - this.last);
+    this.last = this.now;
+    this.minMs = Math.min(this.minMs, msValue);
+    this.maxMs = Math.max(this.maxMs, msValue);
+    this.#drawPanelData(
+      ms.data.data,
+      Math.min(30, 30 - (msValue / 200) * 30),
+      Stats.colorSchemes.ms
+    );
+    ms.text.innerHTML = `<strong>${msValue} MS</strong>(${this.minMs}-${this.maxMs})`;
+    ms.ctx.putImageData(ms.data, 0, 0);
+
+    if (this.now < this.lastFrame + 1000) return; // exit early if less than a second since last update.
+
+    // Calculating FPS.
+    const fpsValue = Math.round((this.framesThisSec * 1000) / (this.now - this.lastFrame));
+    this.minFps = Math.min(this.minFps, fpsValue);
+    this.maxFps = Math.max(this.maxFps, fpsValue);
+    this.#drawPanelData(
+      fps.data.data,
+      Math.min(30, 30 - (fpsValue / 100) * 30),
+      Stats.colorSchemes.fps
+    );
+    fps.text.innerHTML = `<strong>${fpsValue} FPS</strong> (${this.minFps}-${this.maxFps})`;
+    fps.ctx.putImageData(fps.data, 0, 0);
+
+    this.lastFrame = this.now;
+    this.framesThisSec = 0;
+    if (this.#maxPanels !== 3) return;
+
+    // Calculating size of memory used by the heap.
+    const memValue = Math.round(performance.memory.usedJSHeapSize * 9.54e-7);
+    this.minMem = Math.min(this.minMem, memValue);
+    this.maxMem = Math.max(this.maxMem, memValue);
+    this.#drawPanelData(mem.data.data, Math.min(30, 30 - memValue / 2), Stats.colorSchemes.mem);
+    mem.text.innerHTML = `<strong>${memValue} MEM</strong> (${this.minMem}-${this.maxMem})`;
+    mem.ctx.putImageData(mem.data, 0, 0);
+  }
 }
